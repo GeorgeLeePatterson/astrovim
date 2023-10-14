26e6b0e4bc30a53902a519952e1f630ad5d066ec
George Patterson <patterson.george@gmail>
2023-10-14 16:20:32 -0400

    fix: small updates to lua ls

--- lsp/config/lua.lua
+++ lsp/config/lua.lua
@@ -2,15 +2,35 @@ return {
   completion = {
     callSnippet = "Both",
     displayContext = 3,
-    completion = {
-      keywordSnippet = "Both",
-    },
+    keywordSnippet = "Both",
     hint = {
       enable = true,
     },
   },
-  runtime = {
-    version = "LuaJIT",
+  diagnostics = {
+    disable = { "incomplete-signature-doc", "trailing-space" },
+    groupSeverity = {
+      strong = "Warning",
+      strict = "Warning",
+    },
+    groupFileStatus = {
+      ["ambiguity"] = "Opened",
+      ["await"] = "Opened",
+      ["codestyle"] = "None",
+      ["duplicate"] = "Opened",
+      ["global"] = "Opened",
+      ["luadoc"] = "Opened",
+      ["redefined"] = "Opened",
+      ["strict"] = "Opened",
+      ["strong"] = "Opened",
+      ["type-check"] = "Opened",
+      ["unbalanced"] = "Opened",
+      ["unused"] = "Opened",
+    },
+    unusedLocalExclude = { "_*" },
+  },
+  doc = {
+    privateName = { "^_" },
   },
   telemetry = { enable = false },
 }
