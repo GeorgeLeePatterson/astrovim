704ec9a1885f248f03ef166ca736b8a9063f9aaa
George Patterson <patterson.george@gmail.com>
2023-11-06 07:21:22 -0500

    fix: adds neovide settings

--- options.lua
+++ options.lua
@@ -110,12 +110,24 @@ local options = {
   },
 }
 
+-- Helper function for transparency formatting
+local alpha = function()
+  return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+end
+
 -- neovide specific
 if vim.g.neovide then
   options.opt.guifont = "JetBrainsMono Nerd Font,VictorMono Nerd Font"
   options.g.neovide_input_macos_alt_is_meta = true
   options.g.neovide_cursor_vfx_mode = "railgun"
   -- options.g.neovide_scale_factor = 0.3
+
+  -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+  options.g.neovide_transparency = 0.0
+  options.g.transparency = 0.8
+  options.g.neovide_floating_blur_amount_x = 2.0
+  options.g.neovide_floating_blur_amount_y = 2.0
+  -- options.g.neovide_background_color = "#0f1117" .. alpha()
 end
 
 -- If you need more control, you can use the function()...end notation
